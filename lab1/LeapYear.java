21c07933fd90e42ed8083c8e612f5e5c76f4e7be
jiangrui <jiangruitc@163.com>
2018-05-17 16:20:46 +0800

    update the LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Deraiven
  */
 public class LeapYear {
 
@@ -12,6 +12,16 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+    /** Calls isLeapYear to judge the input is conrespond with the require 
+     *  @param  year to be analyzed
+     */
+    public static boolean isLeapYear(int year){
+    	if ((year % 4==0 && year % 100==0) || year % 400==0){
+    		return true;
+    	}else{
+    		return false;
+    	}
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
